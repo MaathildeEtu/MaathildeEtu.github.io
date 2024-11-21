1ff7aa8cb67c92a8baebe6a3df27a9d084f773f3
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-11-21 09:11:52 +0100

    head

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'head': "linear-gradient(180deg, rgba(196,223,216,0.8) 0%, rgba(243,243,243,1) 100%), url('/img-header-portfolio-mathildedecoux.png')",
+			},
+		},
 	},
 	plugins: [],
 }
