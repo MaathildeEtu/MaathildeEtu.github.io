7c467618fa017789de1467574133d7edbf8c8d25
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-11-21 11:10:06 +0100

    Navigation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,29 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'head': "linear-gradient(180deg, rgba(196,223,216,0.8) 0%, rgba(243,243,243,1) 100%), url('/img-header-portfolio-mathildedecoux.png')",
+				'head': "linear-gradient(180deg, rgba(196,223,216,0.8) 0%, rgba(243,243,243) 100%), url('/img-header-portfolio-mathildedecoux.png')",
+				'gradient-head-green-light': "linear-gradient(180deg, rgba(196,223,216,0.8) 0%, rgba(243,243,243) 100%)",
+				'gradient-head-green-dark': "linear-gradient(180deg, rgba(0,115,104,1) 0%, rgba(243,243,243) 100%)",
+				'gradient-head-blue-light': "linear-gradient(180deg, rgba(177,208,229,1) 0%, rgba(243,243,243) 100%)",
+			},
+			fontFamily: {
+				'league-script': 'League Script, cursive',
+				'ephesis': 'Ephesis, cursive;',
+			},
+			colors: {
+				'moutarde-light' : '#D9A324',
+				'moutarde-dark' : '#B5881E',
+				'yellow-light' : '#FBC966',
+				'yellow-dark' : '#D0B577',
+				'beige-light' : '#F9D98E',
+				'beige-dark' : '#D0B577',
+				'green-light-800' : '#007368',
+				'green-light-200' : '#C4DFD8',
+				'green-dark-800' : '#006057',
+				'green-dark-200' : '#A4BAB4',
+				'blue-light' : '#B1D0E5',
+				'blue-dark' : '#94AEBF',
+				'white-light' : 'rgba(243,243,243)',
 			},
 		},
 	},
