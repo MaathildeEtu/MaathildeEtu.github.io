074bdf4d3036b6f17303569cccd958654c9e20c5
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-12-02 09:25:00 +0100

    Blog Partie 1

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,5 @@ export const SITE_TITLE = 'Mathilde Portfolio';
 export const SITE_DESCRIPTION = 'Bienvenue sur mon portfolio';
 export const SITE_URL = 'https://maathildeetu.github.io';
 export const SITE_CONTACT = 'Contact';
-export const SITE_BLOG = 'Blog';
+export const SITE_BLOG = 'My Blog';
 export const SITE_ABOUT = 'About Me';
