41fa7fdd399c29fcafbcb83b828081e910576d17
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2025-05-06 16:37:44 +0200

    Mise en place animation introduction

--- public/script/script.js
+++ public/script/script.js
@@ -1,20 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const intro = document.getElementById('intro');
-    const text = document.getElementById('intro-text');
-    const title = document.getElementById('intro-title');
+	// Sélection des éléments
+	const intro = document.getElementById('intro');
+	const text = document.getElementById('intro-text');
+	const title = document.getElementById('intro-title');
+	const tags = document.getElementById('intro-tags');
 
-    // Animation de l'ensemble
-    intro.classList.remove('opacity-0', 'translate-y-10');
-    intro.classList.add('opacity-100', 'translate-y-0');
+	// Animation du bloc principal
+	intro.classList.remove('opacity-0', 'translate-y-10');
+	intro.classList.add('opacity-100', 'translate-y-0');
 
-    // Animation des textes
-    setTimeout(() => {
-        text.classList.remove('opacity-0', 'translate-y-4');
-        text.classList.add('opacity-100', 'translate-y-0');
-    }, 300);
+	// Animation du texte "Mathilde Decoux"
+	setTimeout(() => {
+		text.classList.remove('opacity-0', 'translate-y-4');
+		text.classList.add('opacity-100', 'translate-y-0');
+	}, 300);
 
-    setTimeout(() => {
-        title.classList.remove('opacity-0', 'translate-y-4');
-        title.classList.add('opacity-100', 'translate-y-0');
-    }, 600);
+	// Animation du titre "Portfolio"
+	setTimeout(() => {
+		title.classList.remove('opacity-0', 'translate-y-4');
+		title.classList.add('opacity-100', 'translate-y-0');
+	}, 600);
+
+	// Animation de la liste de tags
+	setTimeout(() => {
+		tags.classList.remove('opacity-0', 'translate-y-4');
+		tags.classList.add('opacity-100', 'translate-y-0');
+	}, 900);
 });
